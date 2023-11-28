60736d925de04fd34a891befd033a3b3f3195b11
antoinenry <antoinenry@gmail.com>
2023-11-28 18:11:29 -0500

    Button inputs deserialization

---
+++ Assets/Scripts/Input/ButtonCountData.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+[Serializable]
+public struct ButtonCountData
+{
+    public string buttonID;
+    public int totalInput;
+
+    [JsonPropertyName("name")] public string ButtonID { get => buttonID; set => buttonID = value; }
+    [JsonPropertyName("count")] public int InputCount { get => totalInput; set => totalInput = value; }
+
+    public static ButtonCountData[] Deserialize(string json)
+    {
+        return JsonSerializer.Deserialize<ButtonCountData[]>(json);
+    }
+}
--- Assets/Scripts/Input/InputSystem.cs
+++ Assets/Scripts/Input/InputSystem.cs
@@ -1,14 +1,16 @@
 using UnityEngine;
 
+// Behaviour to get input from the server and present it in a convenient way
 public class InputSystem : MonoBehaviour
 {
     public string buttonsRequestUri = "/buttons";
     public float minimumRequestTime = .2f;
     public float maxRequestTime = 1f;
-    public string response;
 
     private WebRequest buttonsRequest;
+    private ButtonCountData[] buttonInputs;
 
+    public float RequestTime { get; private set; }
 
     private void Awake()
     {
@@ -35,12 +37,10 @@ public class InputSystem : MonoBehaviour
                 SendButtonRequest();
                 break;
             case WebRequest.RequestStatus.Running:
-                // Wait while request is running
-                if (Time.time < buttonsRequest.StartTime + maxRequestTime)
-                    response = "(wait)";
                 // Request timeout: notify and relaunch
-                else
+                if (buttonsRequest.Duration > maxRequestTime)
                 {
+                    RequestTime = buttonsRequest.Duration;
                     Debug.LogWarning("Button request timeout");
                     CancelButtonRequest();
                     SendButtonRequest();
@@ -49,7 +49,10 @@ public class InputSystem : MonoBehaviour
             case WebRequest.RequestStatus.Success:
                 // Requess succest: get result, wait and relaunch
                 if (Time.time < buttonsRequest.StartTime + minimumRequestTime)
-                    response = buttonsRequest.ResponseBody;
+                {
+                    RequestTime = buttonsRequest.Duration;
+                    ProcessButtonRequestResponse();
+                }
                 else
                     SendButtonRequest();
                 break;
@@ -65,6 +68,12 @@ public class InputSystem : MonoBehaviour
 
     private void CancelButtonRequest()
     {
+        buttonsRequest.Cancel();
+    }
 
+    private void ProcessButtonRequestResponse()
+    {
+        string response = buttonsRequest.ResponseBody;
+        buttonInputs = ButtonCountData.Deserialize(response);
     }
 }
