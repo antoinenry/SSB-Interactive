01ffca297f4d9a161a53a433b840f9a2c4db3317
antoinenry <antoinenry@gmail.com>
2023-11-28 15:11:16 -0500

    Basic Buttons Getter

--- Assets/Scripts/Input/InputSystem.cs
+++ Assets/Scripts/Input/InputSystem.cs
@@ -3,9 +3,12 @@ using UnityEngine;
 public class InputSystem : MonoBehaviour
 {
     public string buttonsRequestUri = "/buttons";
-    public float minimumRequestTime = 0f;
+    public float minimumRequestTime = .2f;
+    public float maxRequestTime = 1f;
+    public string response;
 
     private WebRequest buttonsRequest;
+
 
     private void Awake()
     {
@@ -14,19 +17,54 @@ public class InputSystem : MonoBehaviour
 
     private void Update()
     {
-        if (buttonsRequest.Status == WebRequest.RequestStatus.Created || Time.time > buttonsRequest.StartTime + minimumRequestTime)
-            GetButtons();
+        GetButtons();
     }
 
     private void GetButtons()
     {
-        if (buttonsRequest.Status != WebRequest.RequestStatus.Running)
+        switch (buttonsRequest.Status)
         {
-            if (buttonsRequest.Status == WebRequest.RequestStatus.Success)
-                Debug.Log("Got Buttons in " + buttonsRequest.Duration + "s");
-            buttonsRequest.requestUri = buttonsRequestUri;
-            buttonsRequest.type = WebRequest.RequestType.GET;
-            if (WebClient.current != null) WebClient.current.SendRequest(buttonsRequest);
+            case WebRequest.RequestStatus.Created:
+                // Launch request for the first time
+                SendButtonRequest();
+                break;
+            case WebRequest.RequestStatus.Failed:
+                // Failed: notify and relaunch
+                Debug.LogWarning("Button request timeout");
+                CancelButtonRequest();
+                SendButtonRequest();
+                break;
+            case WebRequest.RequestStatus.Running:
+                // Wait while request is running
+                if (Time.time < buttonsRequest.StartTime + maxRequestTime)
+                    response = "(wait)";
+                // Request timeout: notify and relaunch
+                else
+                {
+                    Debug.LogWarning("Button request timeout");
+                    CancelButtonRequest();
+                    SendButtonRequest();
+                }
+                break;
+            case WebRequest.RequestStatus.Success:
+                // Requess succest: get result, wait and relaunch
+                if (Time.time < buttonsRequest.StartTime + minimumRequestTime)
+                    response = buttonsRequest.ResponseBody;
+                else
+                    SendButtonRequest();
+                break;
         }
+    }
+
+    private void SendButtonRequest()
+    {
+        buttonsRequest.requestUri = buttonsRequestUri;
+        buttonsRequest.type = WebRequest.RequestType.GET;
+        if (WebClient.current != null) WebClient.current.SendRequest(buttonsRequest);
+    }
+
+    private void CancelButtonRequest()
+    {
+
     }
 }
