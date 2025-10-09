035f82dd5b5f77ae3fbb455c0ba9c7784a43a15c
Antoine Henry <antoinenry@gmail.com>
2025-10-09 18:30:42 +0200

    Setting shop inventory
    Still not plugged to server, but ready

--- Assets/Scripts/Stages/Shop/ShopStage.cs
+++ Assets/Scripts/Stages/Shop/ShopStage.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Shop
@@ -15,14 +16,17 @@ namespace Shop
     {
         public ShopItem[] inventory;
         public ShopShelfDisplay[] shelfDisplays;
+        public HttpRequestLoop songPoolRequest;
 
         private PollMaster poll;
+        private Coroutine refreshInventoryCoroutine;
 
-        protected override bool HasAllComponents()
+        public HttpClientScriptable HttpClient { get; private set; }
+
+        protected override void Awake()
         {
-            if (base.HasAllComponents() && poll) return true;
-            poll = GetComponentInChildren<PollMaster>(true);
-            return base.HasAllComponents() && poll;
+            base.Awake();
+            HttpClient = CurrentAssetsManager.GetCurrent<HttpClientScriptable>();
         }
 
         protected override void OnEnable()
@@ -39,6 +43,59 @@ namespace Shop
 
         private void Update()
         {
+            UpdateShelves();
+        }
+
+        protected override bool HasAllComponents()
+        {
+            if (base.HasAllComponents() && poll) return true;
+            poll = GetComponentInChildren<PollMaster>(true);
+            return base.HasAllComponents() && poll;
+        }
+
+        public void RefreshInventory()
+        {
+            if (refreshInventoryCoroutine == null) refreshInventoryCoroutine = StartCoroutine(RequestAvailableSongsCoroutine());
+        }
+
+        public void CancelRefreshInventory()
+        {
+            StopCoroutine(refreshInventoryCoroutine);
+            refreshInventoryCoroutine = null;
+        }
+
+        public bool Refreshing => refreshInventoryCoroutine != null;
+
+        private IEnumerator RequestAvailableSongsCoroutine()
+        {
+            // Send request for available songs
+            if (songPoolRequest == null)
+            {
+                inventory = new ShopItem[0];
+                refreshInventoryCoroutine = null;
+                yield break;
+            }
+            songPoolRequest.Init();
+            while (songPoolRequest.RequestStatus == HttpRequest.RequestStatus.Running)
+            {
+                songPoolRequest.Update();
+                yield return null;
+            }
+            // Process response
+            string[] availableSongs;
+            if (songPoolRequest.RequestStatus == HttpRequest.RequestStatus.Success)
+                availableSongs = songPoolRequest.DeserializeResponse<string[]>();
+            else
+                availableSongs = new string[0];
+            // Convert available songs into shop items
+            if (availableSongs != null)
+                inventory = Array.ConvertAll(availableSongs, s => CreateShopItem(s));
+            else
+                inventory = new ShopItem[0];
+        }
+
+        private void UpdateShelves()
+        {
             int shelfCount = shelfDisplays != null ? shelfDisplays.Length : 0,
                 inventoryCount = inventory != null ? inventory.Length : 0;
             if (shelfCount == 0) return;
@@ -51,6 +108,14 @@ namespace Shop
                 inventory_index++;
             }
         }
+
+        private ShopItem CreateShopItem(string songTitle)
+            => new ShopItem()
+            {
+                name = songTitle,
+                icon = null,
+                price = 0
+            };
 
         private void OnPollWinner(PollMaster.Candidate candidate)
         {
