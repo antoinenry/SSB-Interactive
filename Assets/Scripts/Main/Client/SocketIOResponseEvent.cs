25c004ecbf9c925e591fb4d22f938d461272c531
Antoine Henry <antoinenry@gmail.com>
2025-10-20 12:16:50 +0200

    Socket response deserialization
    Redundant with HttpRequestLoop ?

--- Assets/Scripts/Main/Client/SocketIOResponseEvent.cs
+++ Assets/Scripts/Main/Client/SocketIOResponseEvent.cs
@@ -1,6 +1,8 @@
+using UnityEngine;
 using UnityEngine.Events;
 using SocketIOClient;
 using System;
+using System.Text.Json;
 
 [Serializable]
 public class SocketIOResponseEvent : UnityEvent<string, SocketIOResponse>
@@ -13,4 +15,19 @@ public class SocketIOResponseEvent : UnityEvent<string, SocketIOResponse>
     }
 
     public void Invoke(SocketIOResponse r) => Invoke(Name, r);
+
+    public static T DeserializeResponse<T>(string response)
+    {
+        if (response == null || response.Length == 0) return default;
+        try
+        {
+            return JsonSerializer.Deserialize<T>(response);
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning("Deserialize exception: " + e);
+            Debug.Log("...when deserializing " + response);
+            return default;
+        }
+    }
 }
