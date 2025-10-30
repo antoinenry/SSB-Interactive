855988c915210b987c4e0abf7127f28d70c3b65c
John Doe <ntnmarchais@gmail.com>
2025-10-30 17:57:21 +0100

    add duration in messenger admin

--- Assets/Scripts/Main/Admin/MessengerAdmin.cs
+++ Assets/Scripts/Main/Admin/MessengerAdmin.cs
@@ -1,9 +1,27 @@
+using System;
+using System.Text.Json;
+using UnityEditor.UIElements;
 using UnityEngine;
+
+[Serializable]
+public struct AdminText
+{
+    public string text;
+    public float duration;
+
+    public AdminText(string text, float duration)
+    {
+        this.text = text;
+        this.duration = duration;
+    }
+}
 
 public class MessengerAdmin : MonoBehaviourSingleton<MessengerAdmin>
 {
     public string eventName = "text";
     public string text;
+
+    public float duration = 4;
     public ObjectMethodCaller methodCaller = new ObjectMethodCaller("Send");
 
     private SocketIOClientScriptable client;
@@ -22,18 +40,30 @@ public class MessengerAdmin : MonoBehaviourSingleton<MessengerAdmin>
 
     public void Send()
     {
-        SendText(text);
+        SendText(text, duration);
     }
 
-    public void SendText(string t)
+    public void SendText(string t, float duration)
     {
         text = t;
         if (!HasAllComponents()) return;
-        client.Emit(eventName, text);
+        AdminText message = new(t, duration);
+        client.Emit(eventName, JsonSerializer.Serialize(message));
     }
 
     static public void Send(string t)
     {
-        Current?.SendText(t);
+        if (Current != null)
+        {
+            Current.SendText(t, Current.duration);
+        }
+    }
+
+    static public void Send(string t, float customDuration)
+    {
+        if (Current != null)
+        {
+            Current.SendText(t, customDuration);
+        }
     }
 }
