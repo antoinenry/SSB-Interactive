b3d16d40b3bc7854a5180d6b6dbb133275e1381a
Antoine Henry <antoinenry@gmail.com>
2025-11-07 10:21:25 +0100

    DK Stage ameliorations
    - Uses input velocity for movements
    - Uses input counter for drop
    - Randomize start position and rotation

--- Assets/_PROTO/Assets/Scripts/DonkeyKong/PropDrop.cs
+++ Assets/_PROTO/Assets/Scripts/DonkeyKong/PropDrop.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.Events;
+using System;
 
 public class PropDrop : MonoBehaviour
 {
@@ -13,12 +14,15 @@ public class PropDrop : MonoBehaviour
 
     private Rigidbody2D loadedProp;
     private AudienceButtonListenerGroup buttonGroup;
+    private AudienceButtonListener dropButton;
 
     public UnityEvent<Rigidbody2D> onDropProp;
 
     private void Awake()
     {
         buttonGroup = GetComponentInChildren<AudienceButtonListenerGroup>(true);
+        if (buttonGroup?.buttons != null)
+            dropButton = Array.Find(buttonGroup.buttons, b => b.buttonID == AudienceInputSource.Current.verticalAxis.negativeButtonID);
     }
 
     private void OnEnable()
@@ -29,6 +33,7 @@ public class PropDrop : MonoBehaviour
             horizontalSpeed = config.horizontalSpeed;
             rotateSpeed = config.rotateSpeed;
         }
+        if (dropButton) dropButton.onValueMaxed.AddListener(DropProp);
     }
 
     private void OnDisable()
@@ -37,6 +42,7 @@ public class PropDrop : MonoBehaviour
         {
             Destroy(loadedProp.gameObject);
         }
+        if (dropButton) dropButton.onValueMaxed.RemoveListener(DropProp);
     }
 
     private void FixedUpdate()
@@ -48,11 +54,12 @@ public class PropDrop : MonoBehaviour
         }
         else
         {
-            float hInput = AudienceInputSource.Current.GetHorizontalAxis().deltaPresses;
-            float vInput = AudienceInputSource.Current.GetVerticalAxis().deltaPresses;
-            transform.position += hInput * deltaTime * horizontalSpeed * Vector3.right;
-            if (vInput > 0f) loadedProp.transform.rotation *= Quaternion.AngleAxis(rotateSpeed * Time.fixedDeltaTime, Vector3.forward);
-            else if (vInput < 0f) DropProp();
+            AudienceInputSource inputSource = AudienceInputSource.Current;
+            if (inputSource == null) return;
+            float horizontalInput = inputSource.GetHorizontalAxis().velocity;
+            float rotationInput = inputSource.GetButton(inputSource.verticalAxis.positiveButtonID).velocity;
+            if (horizontalInput != 0f) transform.position += horizontalInput * deltaTime * horizontalSpeed * Vector3.right;
+            if (rotationInput > 0f) loadedProp.transform.rotation *= Quaternion.AngleAxis(rotationInput * rotateSpeed * Time.fixedDeltaTime, Vector3.forward);
         }
     }
 
@@ -60,7 +67,7 @@ public class PropDrop : MonoBehaviour
     {
         int chanceSum = 0;
         foreach (int c in chances) chanceSum += c;
-        float random = Random.Range(0, chanceSum);
+        float random = UnityEngine.Random.Range(0, chanceSum);
         int propIndex = 0;
         chanceSum = 0;
         for (int i = 0; i < propPrefabs.Length && i < chances.Length; i++)
@@ -77,10 +84,10 @@ public class PropDrop : MonoBehaviour
         if (randomizeStartTransform)
         {
             Vector3 pos = transform.position;
-            pos.x = Random.Range(-horizontalRange, horizontalRange);
+            pos.x = UnityEngine.Random.Range(-horizontalRange, horizontalRange);
             transform.position = pos;
             Vector3 rot = transform.rotation.eulerAngles;
-            rot.z = Random.Range(0f, 360f);
+            rot.z = UnityEngine.Random.Range(0f, 360f);
             loadedProp.transform.rotation = Quaternion.Euler(rot);
         }
         loadedProp.simulated = false;
